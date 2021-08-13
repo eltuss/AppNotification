19e7cd70578c1478c672fd9887b3acb822e1f71f
Eltuss <elton.s.barbosa@outlook.com>
2021-08-13 19:01:11 -0300

    first commit

---
+++ app/src/main/java/com/example/appnotification/NotificationUtils.kt
@@ -0,0 +1,48 @@
+package com.example.appnotification
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+
+fun Context.showNotification(channelId: String, title: String, body: String) {
+
+    val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    val intent = Intent(this, MainActivity::class.java)
+    val pendingIntent =
+        PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    val notificationBuilder: NotificationCompat.Builder =
+        NotificationCompat.Builder(this, channelId).apply {
+            setSmallIcon(R.drawable.ic_launcher_foreground)
+            setContentTitle(title)
+            setContentText(body)
+            setAutoCancel(true)
+            setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE))
+            setContentIntent(pendingIntent)
+        }
+
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val notificationChannel =
+            NotificationChannel(channelId, body, NotificationManager.IMPORTANCE_HIGH).apply {
+                lightColor = Color.BLUE
+                enableVibration(true)
+            }
+        notificationManager.createNotificationChannel(notificationChannel)
+
+    }
+    notificationManager.notify(channelId.toInt(), notificationBuilder.build())
+
+}
+
+
+
